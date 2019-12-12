95f4920697904237761c98235b544da0fb9e9993
傅立业 <17433201@qq.com>
2019-12-12 22:36:02 +0800

    Add Q51

---
+++ LeetCode-Swift.playground/Pages/51. N Queens.xcplaygroundpage/Contents.swift
@@ -0,0 +1,21 @@
+//: [Previous](@previous)
+
+class Solution {
+	func solveNQueens(_ n: Int) -> [[String]] {
+		// FIXME: ...
+		fatalError()
+	}
+}
+
+assert(
+	Set(Solution().solveNQueens(4))
+	==
+	Set(
+		[
+			[".Q..", "...Q", "Q...", "..Q."],
+			["..Q.", "Q...", "...Q", ".Q.."],
+		]
+	)
+)
+
+//: [Next](@next)
