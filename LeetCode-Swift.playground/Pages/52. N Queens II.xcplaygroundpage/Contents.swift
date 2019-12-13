36cc4457ac7e202ea1701636863559648af03254
傅立业 <17433201@qq.com>
2019-12-13 23:31:12 +0800

    Solve Q52

--- LeetCode-Swift.playground/Pages/52. N Queens II.xcplaygroundpage/Contents.swift
+++ LeetCode-Swift.playground/Pages/52. N Queens II.xcplaygroundpage/Contents.swift
@@ -1,9 +1,36 @@
 //: [Previous](@previous)
 
 class Solution {
-	func totalNQueens(_ n: Int) -> Int {
-		// FIXME: ...
-		fatalError()
+	func totalNQueens(_ n: Int, _ columnIndexes: [Int] = []) -> Int {
+		let currentRowIndex = columnIndexes.count
+		
+		if currentRowIndex == n {
+			return 1
+		}
+		
+		var solutionCount: Int = 0
+		
+		LOOP_CURRENT_COLUMN_INDEX:
+		for currentColumnIndex in 0 ..< n {
+			for rowIndex in 0 ..< currentRowIndex {
+				let columnIndex = columnIndexes[rowIndex]
+				
+				if (
+					currentColumnIndex == columnIndex
+					||
+					abs(currentColumnIndex - columnIndex) == currentRowIndex - rowIndex
+				) {
+					continue LOOP_CURRENT_COLUMN_INDEX
+				}
+			}
+			
+			var columnIndexes = columnIndexes
+			columnIndexes.append(currentColumnIndex)
+			
+			solutionCount += totalNQueens(n, columnIndexes)
+		}
+		
+		return solutionCount
 	}
 }
 
