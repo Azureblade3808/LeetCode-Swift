ff084cef6f972cc6fd53c9779e4040f9dc6d9eef
傅立业 <17433201@qq.com>
2019-12-13 23:15:02 +0800

    Solve Q51

--- LeetCode-Swift.playground/Pages/51. N Queens.xcplaygroundpage/Contents.swift
+++ LeetCode-Swift.playground/Pages/51. N Queens.xcplaygroundpage/Contents.swift
@@ -2,8 +2,43 @@
 
 class Solution {
 	func solveNQueens(_ n: Int) -> [[String]] {
-		// FIXME: ...
-		fatalError()
+		let solutionsAsIntArrays = solveNQueensAsIntArrays(n)
+		let solutions = solutionsAsIntArrays.map { intArray in
+			intArray.map { index in
+				"\(String(repeating: ".", count: index))Q\(String(repeating: ".", count: n - index - 1))"
+			}
+		}
+		return solutions
+	}
+	
+	private func solveNQueensAsIntArrays(_ n: Int, _ columnIndexes: [Int] = []) -> [[Int]] {
+		let currentRowIndex = columnIndexes.count
+		
+		if currentRowIndex == n {
+			return [columnIndexes]
+		}
+		
+		var solutions: [[Int]] = []
+		
+		LOOP_CURRENT_COLUMN_INDEX:
+		for currentColumnIndex in 0 ..< n {
+			for (rowIndex, columnIndex) in columnIndexes.enumerated() {
+				if currentColumnIndex == columnIndex {
+					continue LOOP_CURRENT_COLUMN_INDEX
+				}
+				
+				if abs(currentColumnIndex - columnIndex) == currentRowIndex - rowIndex {
+					continue LOOP_CURRENT_COLUMN_INDEX
+				}
+			}
+			
+			var columnIndexes = columnIndexes
+			columnIndexes.append(currentColumnIndex)
+			
+			solutions += solveNQueensAsIntArrays(n, columnIndexes)
+		}
+		
+		return solutions
 	}
 }
 
