8a51ad900aec5c2cc9939ec301cec4432cfc6adc
傅立业 <17433201@qq.com>
2019-12-13 23:19:09 +0800

    Add Q52

---
+++ LeetCode-Swift.playground/Pages/52. N Queens II.xcplaygroundpage/Contents.swift
@@ -0,0 +1,15 @@
+//: [Previous](@previous)
+
+class Solution {
+	func totalNQueens(_ n: Int) -> Int {
+		// FIXME: ...
+		fatalError()
+	}
+}
+
+assert(Solution().totalNQueens(1) == 1)
+assert(Solution().totalNQueens(2) == 0)
+assert(Solution().totalNQueens(3) == 0)
+assert(Solution().totalNQueens(4) == 2)
+
+//: [Next](@next)
