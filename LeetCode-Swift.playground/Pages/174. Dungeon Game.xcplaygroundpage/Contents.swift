9dc89736f98234d4379c0b4aafa6a7fca9c7b86b
傅立业 <17433201@qq.com>
2019-12-14 23:18:02 +0800

    Add Q174

---
+++ LeetCode-Swift.playground/Pages/174. Dungeon Game.xcplaygroundpage/Contents.swift
@@ -0,0 +1,15 @@
+//: [Previous](@previous)
+
+class Solution {
+	func calculateMinimumHP(_ dungeon: [[Int]]) -> Int {
+		// FIXME: ...
+		fatalError()
+	}
+}
+
+let calculateMinimumHP = Solution().calculateMinimumHP
+assert(calculateMinimumHP([[-2, -2, 3], [-5, -10, 1], [10, 30, 5]]) == 7)
+
+print("OK")
+
+//: [Next](@next)
