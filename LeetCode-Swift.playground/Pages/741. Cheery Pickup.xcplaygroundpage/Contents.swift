0a14b647060a7f6f590e547505a90986ceff1ab4
傅立业 <17433201@qq.com>
2019-12-15 20:38:45 +0800

    Add a test case for Q741

--- LeetCode-Swift.playground/Pages/741. Cheery Pickup.xcplaygroundpage/Contents.swift
+++ LeetCode-Swift.playground/Pages/741. Cheery Pickup.xcplaygroundpage/Contents.swift
@@ -60,6 +60,7 @@ class Solution {
 let cherryPickup = Solution().cherryPickup
 assert(cherryPickup([[0, 1, -1], [1, 0, -1], [1, 1,  1]]) == 5)
 assert(cherryPickup([[1, 1, -1], [1, -1, 1], [-1, 1, 1]]) == 0)
+assert(cherryPickup([[1, -1, -1, -1, -1], [1, 0, 1, -1, -1], [0, -1, 1, 0, 1], [1, 0, 1, 1, 0], [-1, -1, -1, 1, 1]]) == 10)
 
 print("OK")
 
