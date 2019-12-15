e4660c0d22af18f997cee0125914d2fd234d273c
傅立业 <17433201@qq.com>
2019-12-15 14:44:21 +0800

    Add Q741

---
+++ LeetCode-Swift.playground/Pages/741. Cheery Pickup.xcplaygroundpage/Contents.swift
@@ -0,0 +1,15 @@
+//: [Previous](@previous)
+
+class Solution {
+	func cherryPickup(_ grid: [[Int]]) -> Int {
+		// FIXME: ...
+		fatalError()
+	}
+}
+
+let cherryPickup = Solution().cherryPickup
+assert(cherryPickup([[0, 1, -1], [1, 0, -1], [1, 1,  1]]) == 5)
+
+print("OK")
+
+//: [Next](@next)
