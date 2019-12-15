8faa9626f90fdec5a163df0026c3d0a979ff3fc6
傅立业 <17433201@qq.com>
2019-12-15 13:28:19 +0800

    Solve Q174

--- LeetCode-Swift.playground/Pages/174. Dungeon Game.xcplaygroundpage/Contents.swift
+++ LeetCode-Swift.playground/Pages/174. Dungeon Game.xcplaygroundpage/Contents.swift
@@ -3,56 +3,56 @@
 class Solution {
 	func calculateMinimumHP(_ dungeon: [[Int]]) -> Int {
 		let height = dungeon.count
+		assert(height > 0)
 		
 		let firstRow = dungeon[0]
 		let width = firstRow.count
+		assert(width > 0)
 		
-		func calculateDeepestDamageOfSafestRoute(
-			_ x: Int,
-			_ y: Int,
-			_ previousTotalValue: Int
-		) -> Int {
+		var cachedBestRoutes: [Int64 : Int] = [:]
+		
+		func calculateBestRoute(_ x: Int, _ y: Int) -> Int {
+			let cacheKey = (Int64(x) << 32) | Int64(y)
+			
+			if let cachedBestRoute = cachedBestRoutes[cacheKey] {
+				return cachedBestRoute
+			}
+			
 			let value = dungeon[y][x]
-			let totalValue = previousTotalValue + value
 			
-			var deepestDamageOfSafestRoute = min(totalValue, 0)
-			
+			let bestRoute: Int
 			if x < width - 1 {
 				if y < height - 1 {
-					let deepestDamageOfSafestRouteFromRightwards = calculateDeepestDamageOfSafestRoute(x + 1, y, totalValue)
-					let deepestDamageOfSafestRouteFromDownwards = calculateDeepestDamageOfSafestRoute(x, y + 1, totalValue)
-					let deepestDamageOfSafestRouteFromNext = max(
-						deepestDamageOfSafestRouteFromRightwards,
-						deepestDamageOfSafestRouteFromDownwards
-					)
-					if deepestDamageOfSafestRouteFromNext < deepestDamageOfSafestRoute {
-						deepestDamageOfSafestRoute = deepestDamageOfSafestRouteFromNext
-					}
+					// Two ways.
+					let bestRouteFromRight = calculateBestRoute(x + 1, y)
+					let bestRouteFromBelow = calculateBestRoute(x, y + 1)
+					let bestRouteFromNext = max(bestRouteFromRight, bestRouteFromBelow)
+					bestRoute = min(value + bestRouteFromNext, 0)
 				}
 				else {
-					let deepestDamageOfSafestRouteFromRightwards = calculateDeepestDamageOfSafestRoute(x + 1, y, totalValue)
-					if deepestDamageOfSafestRouteFromRightwards < deepestDamageOfSafestRoute {
-						deepestDamageOfSafestRoute = deepestDamageOfSafestRouteFromRightwards
-					}
+					// Way right only.
+					let bestRouteFromRight = calculateBestRoute(x + 1, y)
+					bestRoute = min(value + bestRouteFromRight, 0)
 				}
 			}
 			else {
 				if y < height - 1 {
-					let deepestDamageOfSafestRouteFromDownwards = calculateDeepestDamageOfSafestRoute(x, y + 1, totalValue)
-					if deepestDamageOfSafestRouteFromDownwards < deepestDamageOfSafestRoute {
-						deepestDamageOfSafestRoute = deepestDamageOfSafestRouteFromDownwards
-					}
+					// Way down only.
+					let bestRouteFromBelow = calculateBestRoute(x, y + 1)
+					bestRoute = min(value + bestRouteFromBelow, 0)
 				}
 				else {
-					// Do nothing.
+					bestRoute = min(value, 0)
 				}
 			}
 			
-			return deepestDamageOfSafestRoute
+			cachedBestRoutes[cacheKey] = bestRoute
+			
+			return bestRoute
 		}
+		let bestRoute = calculateBestRoute(0, 0)
 		
-		let deepestDamageOfSafestRoute = calculateDeepestDamageOfSafestRoute(0, 0, 0)
-		let minimumHp = 1 - deepestDamageOfSafestRoute
+		let minimumHp = 1 - min(bestRoute, 0)
 		
 		return minimumHp
 	}
@@ -60,6 +60,7 @@ class Solution {
 
 let calculateMinimumHP = Solution().calculateMinimumHP
 assert(calculateMinimumHP([[-2, -3, 3], [-5, -10, 1], [10, 30, -5]]) == 7)
+assert(calculateMinimumHP([[1, 2, 1], [-2, -3, -3], [3, 2, -2]]) == 1)
 
 print("OK")
 
