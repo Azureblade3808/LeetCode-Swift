48f3cd5fb10f6d047052b02702493b316d930aa0
傅立业 <17433201@qq.com>
2019-12-17 22:56:45 +0800

    Add Q5

---
+++ LeetCode-Swift.playground/Pages/5. Longest Palindromic Substring.xcplaygroundpage/Contents.swift
@@ -0,0 +1,14 @@
+//: [Previous](@previous)
+
+class Solution {
+	func longestPalindrome(_ s: String) -> String {
+		// FIXME: ...
+		fatalError()
+	}
+}
+
+let longestPalindrome = Solution().longestPalindrome
+assert(["bab", "aba"].contains(longestPalindrome("babad")))
+assert(longestPalindrome("cbbd") == "bb")
+
+//: [Next](@next)
