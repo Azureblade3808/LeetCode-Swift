7024805a454c8b0f36ea308544f30f570f95098b
傅立业 <17433201@qq.com>
2019-12-17 21:50:15 +0800

    Add Q516

---
+++ LeetCode-Swift.playground/Pages/516. Longest Palindromic Subsequence.xcplaygroundpage/Contents.swift
@@ -0,0 +1,17 @@
+//: [Previous](@previous)
+
+class Solution {
+	func longestPalindromeSubseq(_ s: String) -> Int {
+		assert(0 ... 1000 ~= s.count)
+		
+		// FIXME: ...
+		fatalError()
+	}
+}
+
+let longestPalindromeSubseq = Solution().longestPalindromeSubseq
+assert(longestPalindromeSubseq("cbbd") == 2)
+
+print("OK")
+
+//: [Next](@next)
