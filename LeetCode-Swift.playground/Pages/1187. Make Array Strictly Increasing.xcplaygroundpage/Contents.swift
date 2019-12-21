c4fdce502d641fb57677ad680b6c2374c6d1ca50
傅立业 <17433201@qq.com>
2019-12-21 22:00:52 +0800

    Add Q1187

---
+++ LeetCode-Swift.playground/Pages/1187. Make Array Strictly Increasing.xcplaygroundpage/Contents.swift
@@ -0,0 +1,15 @@
+//: [Previous](@previous)
+
+class Solution {
+	func makeArrayIncreasing(_ array0: [Int], _ array1: [Int]) -> Int {
+		// FIXME: ...
+		fatalError()
+	}
+}
+
+let makeArrayIncreasing = Solution().makeArrayIncreasing
+assert(makeArrayIncreasing([1, 5, 3, 6, 7], [1, 3, 2, 4]) == 1)
+assert(makeArrayIncreasing([1, 5, 3, 6, 7], [4, 3, 1]) == 2)
+assert(makeArrayIncreasing([1, 5, 3, 6, 7], [1, 6, 3, 3]) == -1)
+
+//: [Next](@next)
