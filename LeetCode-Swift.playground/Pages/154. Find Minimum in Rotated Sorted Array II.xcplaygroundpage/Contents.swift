ec6027212ee256cf389d8724cf7f1db89889684a
傅立业 <17433201@qq.com>
2019-12-21 20:17:15 +0800

    Add Q154

---
+++ LeetCode-Swift.playground/Pages/154. Find Minimum in Rotated Sorted Array II.xcplaygroundpage/Contents.swift
@@ -0,0 +1,16 @@
+//: [Previous](@previous)
+
+class Solution {
+	func findMin(_ nums: [Int]) -> Int {
+		// TODO: ...
+		fatalError()
+	}
+}
+
+let findMin = Solution().findMin
+assert(findMin([1,3,5]) == 1)
+assert(findMin([2,2,2,0,1] == 0)
+
+print("OK")
+
+//: [Next](@next)
