618eb7545199c38d08bfe0a5a67bc27b817d0939
傅立业 <17433201@qq.com>
2019-12-21 16:56:48 +0800

    Provide a solution to Q516, accepted
    Character array is used instead of pointer, and the speed is about the same.

--- LeetCode-Swift.playground/Pages/516. Longest Palindromic Subsequence.xcplaygroundpage/Contents.swift
+++ LeetCode-Swift.playground/Pages/516. Longest Palindromic Subsequence.xcplaygroundpage/Contents.swift
@@ -9,58 +9,54 @@ class Solution {
 			return length
 		}
 		
-		return string.withCString { pointer in
-			func charAt(_ index: Int) -> Int8 {
-				return pointer.advanced(by: index).pointee
-			}
+		let characters = Array(string)
+		
+		var cachedResults: [Int : Int] = [:]
+		
+		func calculateResult(_ leftIndex: Int, _ rightIndex: Int) -> Int {
+			assert(leftIndex <= rightIndex)
 			
-			var cachedResults: [Int : Int] = [:]
+			let cacheKey = (leftIndex << 16) | rightIndex
 			
-			func calculateResult(_ leftIndex: Int, _ rightIndex: Int) -> Int {
-				assert(leftIndex <= rightIndex)
-				
-				let cacheKey = (leftIndex << 16) | rightIndex
-				
-				if let result = cachedResults[cacheKey] {
-					return result
-				}
-				
-				var result: Int
-				
-				switch rightIndex - leftIndex {
-					case 0:
-					do {
-						result = 1
-					}
-					
-					case 1:
-					do {
-						if charAt(leftIndex) == charAt(rightIndex) {
-							result = 2
-						}
-						else {
-							result = 1
-						}
-					}
-					
-					default:
-					do {
-						if charAt(leftIndex) == charAt(rightIndex) {
-							result = 2 + calculateResult(leftIndex + 1, rightIndex - 1)
-						}
-						else {
-							result = max(calculateResult(leftIndex + 1, rightIndex), calculateResult(leftIndex, rightIndex - 1))
-						}
-					}
-				}
-				
-				cachedResults[cacheKey] = result
-				
+			if let result = cachedResults[cacheKey] {
 				return result
 			}
 			
-			return calculateResult(0, length - 1)
+			var result: Int
+			
+			switch rightIndex - leftIndex {
+				case 0:
+				do {
+					result = 1
+				}
+				
+				case 1:
+				do {
+					if characters[leftIndex] == characters[rightIndex] {
+						result = 2
+					}
+					else {
+						result = 1
+					}
+				}
+				
+				default:
+				do {
+					if characters[leftIndex] == characters[rightIndex] {
+						result = 2 + calculateResult(leftIndex + 1, rightIndex - 1)
+					}
+					else {
+						result = max(calculateResult(leftIndex + 1, rightIndex), calculateResult(leftIndex, rightIndex - 1))
+					}
+				}
+			}
+			
+			cachedResults[cacheKey] = result
+			
+			return result
 		}
+		
+		return calculateResult(0, length - 1)
 	}
 }
 
