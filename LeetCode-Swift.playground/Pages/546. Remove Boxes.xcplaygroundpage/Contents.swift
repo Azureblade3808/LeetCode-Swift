6ea48281a57b8f8114cfb621e5ea98cd5acc1ffa
傅立业 <17433201@qq.com>
2019-12-23 19:01:22 +0800

    Add Q546

---
+++ LeetCode-Swift.playground/Pages/546. Remove Boxes.xcplaygroundpage/Contents.swift
@@ -0,0 +1,17 @@
+//: [Previous](@previous)
+
+class Solution {
+	func removeBoxes(_ boxes: [Int]) -> Int {
+		assert(boxes.count <= 100)
+		
+		// TODO: ...
+		fatalError()
+	}
+}
+
+let removeBoxes = Solution().removeBoxes
+assert(removeBoxes([1, 3, 2, 2, 2, 3, 4, 3, 1]) == 23)
+
+print("OK")
+
+//: [Next](@next)
